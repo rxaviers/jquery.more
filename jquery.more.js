d266992cf0b9b5f6e05b274ee4750bb25e87f46d
Rafael Xavier de Souza <rxaviers@gmail.com>
2012-05-07 22:53:19 -0300

    Indentation fix

--- jquery.more.js
+++ jquery.more.js
@@ -10,75 +10,74 @@
  *
  * Copyright (c) 2011 Rafael Xavier de Souza (http://rafael.xavier.blog.br)
  */
-;(function( $, undefined ) {
+;
+(function( $, undefined ) {
 
-  var defaults = {
-    more_el: null,
-    bind: "click.more",
-    afterbind: null,
-    trigger: {
-      find: "find",
-      more: "more"
-    },
-    destroy_on_end: true
-  };
+	var defaults = {
+		more_el: null,
+		bind: "click.more",
+		afterbind: null,
+		trigger: {
+			find: "find",
+			more: "more"
+		},
+		destroy_on_end: true
+	};
 
-  function More( element, options ) {
-    this.element = $( element );
-    this.options = options;
-    this._create();
-  };
+	function More(element, options) {
+		this.element = $(element);
+		this.options = options;
+		this._create();
+	};
 
-  More.prototype = {
-    _create: function() {
-      if( !this.options.more_el ) {
-        this.options.more_el = $( "<div></div>" )
-          .addClass( "more-meristem" )
-          .appendTo( this.element );
-      }
-      this._bind();
-    },
+	More.prototype = {
+		_create: function() {
+			if (!this.options.more_el ) {
+				this.options.more_el = $("<div></div>").addClass("more-meristem").appendTo(this.element);
+			}
+			this._bind();
+		},
 
-    _run: function() {
-      var self = this,
-        cb = function() {
-          self._more.apply( self, arguments );
-        };
-      this._unbind();
-      this.element.triggerHandler( this.options.trigger.find, [ cb ] );
-    },
+		_run: function() {
+			var self = this,
+				cb = function() {
+					self._more.apply(self, arguments);
+				};
+			this._unbind();
+			this.element.triggerHandler(this.options.trigger.find, [cb]);
+		},
 
-    _more: function() {
-      if( !this.element.triggerHandler( this.options.trigger.more, arguments ) && this.options.destroy_on_end ) {
-        this.options.more_el.remove();
-        return;
-      }
-      this._bind();
-    },
+		_more: function() {
+			if (!this.element.triggerHandler(this.options.trigger.more, arguments) && this.options.destroy_on_end ) {
+				this.options.more_el.remove();
+				return;
+			}
+			this._bind();
+		},
 
-    _bind: function() {
-      var self = this;
-      this.options.more_el.bind( this.options.bind, function() {
-        self._run.call( self );
-      });
-      if( this.options.afterbind ) {
-        this.options.afterbind.call( this.element[0] );
-      }
-    },
+		_bind: function() {
+			var self = this;
+			this.options.more_el.bind(this.options.bind, function() {
+				self._run.call(self);
+			});
+			if ( this.options.afterbind ) {
+				this.options.afterbind.call(this.element[0]);
+			}
+		},
 
-    _unbind: function() {
-      this.options.more_el.unbind( this.options.bind );
-    }
-  };
+		_unbind: function() {
+			this.options.more_el.unbind(this.options.bind);
+		}
+	};
 
-  $.fn.more = function( options ) {
-    options = $.extend( $.extend( true, {}, $.fn.more.defaults ), options);
-    this.each( function() {
-      new More( this, options );
-    });
-    return this;
-  };
+	$.fn.more = function( options ) {
+		options = $.extend($.extend(true, {}, $.fn.more.defaults), options);
+		this.each(function() {
+			new More(this, options);
+		});
+		return this;
+	};
 
-  $.fn.more.defaults = defaults;
+	$.fn.more.defaults = defaults;
 
-})( jQuery );
+})(jQuery);
